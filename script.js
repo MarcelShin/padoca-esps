51627b2782da527be8a46503d07e77899c833479
˜codethi˜ <thiago.lima3@ab-inbev.com>
2023-11-08 21:05:03 -0300

    crud

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const btnCadastro = document.querySelector("form button#register");
 const btnUpdate = document.querySelector("form button#update");
+const btnCadastrarNovoProduto = document.querySelector("#insert-new-product");
+const formInsert = document.querySelector("#insert-form");
+const listProducts = document.querySelector("#list-products");
+const formUpdate = document.querySelector("#update-form");
 
 btnCadastro.addEventListener("click", registerProduct);
 btnUpdate.addEventListener("click", updateProduct);
+btnCadastrarNovoProduto.addEventListener("click", () => {
+  formInsert.classList.toggle("display-none");
+  formInsert.classList.toggle("display-flex");
+  btnCadastrarNovoProduto.classList.toggle("display-none");
+  listProducts.classList.toggle("display-none");
+});
 
 const products = [];
 
@@ -22,11 +32,16 @@ function registerProduct() {
 
   products.push(product);
 
+  formInsert.classList.toggle("display-none");
+  formInsert.classList.toggle("display-flex");
+  btnCadastrarNovoProduto.classList.toggle("display-none");
+  btnCadastrarNovoProduto.classList.toggle("display-flex");
+  listProducts.classList.toggle("display-none");
+
   showProducts();
 }
 
 function showProducts() {
-  console.log(products);
   const productRows = document.querySelector("#product-rows");
   productRows.innerHTML = "";
   for (let i = 0; i < products.length; i++) {
@@ -61,29 +76,53 @@ function removeProduct(id) {
 }
 
 function getProduct(id) {
-  const formUpdate = document.querySelector("#update-form");
-  const formInsert = document.querySelector("#insert-form");
-  const listProducts = document.querySelector('#list-products')
-
   formUpdate.classList.toggle("display-flex");
   formUpdate.classList.toggle("display-none");
-  
-  formInsert.classList.toggle("display-none");
-  formInsert.classList.toggle("display-flex");
+
+  btnCadastrarNovoProduto.classList.toggle("display-none");
+  btnCadastrarNovoProduto.classList.toggle("display-flex");
 
   listProducts.classList.toggle("display-none");
 
   for (let i = 0; i < products.length; i++) {
     if (products[i].id === id) {
-      document.querySelector("#name-update").value = products[i].name
-      document.querySelector("#price-update").value = products[i].price
-      document.querySelector("#qtd-update").value = products[i].qtd
-      document.querySelector("#description-update").value = products[i].description
+      document.querySelector("#name-update").value = products[i].name;
+      document.querySelector("#price-update").value = products[i].price;
+      document.querySelector("#qtd-update").value = products[i].qtd;
+      document.querySelector("#description-update").value =
+        products[i].description;
+      document.querySelector("#id-product").value = products[i].id;
       break;
     }
   }
 }
 
-function updateProduct() {
+const btnUpdateProduct = document.querySelector("#update");
 
+btnUpdateProduct.addEventListener("click", updateProduct);
+
+function updateProduct() {
+  const name = document.querySelector("#name-update").value;
+  const price = document.querySelector("#price-update").value;
+  const qtd = document.querySelector("#qtd-update").value;
+  const description = document.querySelector("#description-update").value;
+  const id = document.querySelector("#id-product").value;
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === id) {
+      products[i].name = name;
+      products[i].price = price;
+      products[i].qtd = qtd;
+      products[i].description = description;
+      break;
+    }
+  }
+
+  btnCadastrarNovoProduto.classList.toggle("display-none");
+  btnCadastrarNovoProduto.classList.toggle("display-flex");
+  listProducts.classList.toggle("display-none");
+  formUpdate.classList.toggle("display-flex");
+  formUpdate.classList.toggle("display-none");
+
+  showProducts();
 }
